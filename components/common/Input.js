73724652c64cb54bea0d919ddb38dd37c22c2c31
majidhzdh <majidhzdh@gmai.com>
2023-05-01 00:37:53 +0330

    input.js created

--- components/common/Input.js
+++ components/common/Input.js
@@ -1,19 +1,53 @@
-import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components';
 
-const SInput = styled.input`
-    border: none;
-    border-bottom: 2px solid red;
-    &:focus{
-        outline: none;
+const floatingLabelAnimation = keyframes`
+  from {
+    transform: translateY(0);
+    color: black;
+  }
+  to {
+    transform: translateY(-20px);
+    color: red;
+  }
+`;
+
+const InputContainer = styled.div`
+  position: relative;
+  margin: 50px;
+`;
+
+const InputField = styled.input`
+  font-size: 18px;
+  padding: 10px;
+  width: 200px;
+  border: none;
+  border-bottom: 2px solid gray;
+  background-color: transparent;
+  &:focus {
+    border-bottom: 2px solid blue;
+    outline: none;
+    & + label {
+      animation: ${floatingLabelAnimation} 0.3s forwards;
     }
-`
+  }
+`;
 
+const InputLabel = styled.label`
+  position: absolute;
+  pointer-events: none;
+  top: 10px;
+  left: 10px;
+  font-size: 16px;
+  transition: 0.3s;
+`;
 
 const Input = () => {
   return (
-    <SInput type='text' placeholder='input'/>
-  )
-}
+    <InputContainer>
+      <InputField type="text" id="my-input" />
+      <InputLabel htmlFor="my-input">My Label</InputLabel>
+    </InputContainer>
+  );
+};
 
 export default Input
