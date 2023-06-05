362755dafc7dcf30c16603d6336ae3f4f1c1d4a5
MajidHosseinzadeh <MajidHosseinzadehh@gmail.com>
2023-06-05 12:29:23 +0330

    changes

--- components/rare/3D.js
+++ components/rare/3D.js
@@ -3,45 +3,45 @@ import { useRef, useLayoutEffect } from "react"
 import { useTransform, useScroll, useTime } from "framer-motion"
 import { degreesToRadians, progress, mix } from "popmotion"
 import styled from "styled-components"
-
+import * as THREE from 'three'
 const color = "#111111"
 
 
 const Icosahedron = () => (
-    <mesh rotation-x={0.35}>
-        <icosahedronGeometry args={[1, 0]} />
-        <meshBasicMaterial wireframe color={color} />
-    </mesh>
+  <mesh rotation-x={0.35}>
+    <icosahedronGeometry args={[1, 0]} />
+    <meshBasicMaterial wireframe color={color} />
+  </mesh>
 )
 
 
 function Scene() {
-    const gl = useThree(state => state.gl)
-    const { scrollYProgress } = useScroll()
-    const yAngle = useTransform(
-        scrollYProgress,
-        [0, 1],
-        [0.001, degreesToRadians(180)]
+  const gl = useThree(state => state.gl)
+  const { scrollYProgress } = useScroll()
+  const yAngle = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0.001, degreesToRadians(180)]
+  )
+  const distance = useTransform(scrollYProgress, [0, 1], [10, 3])
+  const time = useTime()
+
+  useFrame(({ camera }) => {
+    camera.position.setFromSphericalCoords(
+      distance.get(),
+      yAngle.get(),
+      time.get() * 0.0005
     )
-    const distance = useTransform(scrollYProgress, [0, 1], [10, 3])
-    const time = useTime()
+    camera.updateProjectionMatrix()
+    camera.lookAt(0, 0, 0)
+  })
 
-    useFrame(({ camera }) => {
-        camera.position.setFromSphericalCoords(
-            distance.get(),
-            yAngle.get(),
-            time.get() * 0.0005
-        )
-        camera.updateProjectionMatrix()
-        camera.lookAt(0, 0, 0)
-    })
-
-    useLayoutEffect(() => gl.setPixelRatio(1))
+  useLayoutEffect(() => gl.setPixelRatio(1))
 
 
-    return (
-        <Icosahedron />
-    )
+  return (
+      <Icosahedron />
+  )
 }
 const MyCanvas = styled(Canvas)`
   image-rendering: -moz-crisp-edges;
@@ -58,11 +58,11 @@ const Container = styled.div`
   left: 0;
 `
 export default function App() {
-    return (
-        <Container>
-            <MyCanvas gl={{ antialias: false }}>
-                <Scene />
-            </MyCanvas>
-        </Container>
-    )
+  return (
+    <Container>
+      <MyCanvas gl={{ antialias: false }}>
+        <Scene />
+      </MyCanvas>
+    </Container>
+  )
 }
